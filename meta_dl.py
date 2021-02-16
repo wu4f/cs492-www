d11b49af13668c700889f11126eddad9639ece12
Wu-chang Feng <wuchang@pdx.edu>
2021-02-16 08:43:28 -0800

    update web site

--- meta_dl.py
+++ meta_dl.py
@@ -3,12 +3,13 @@ import sys
 from bs4 import BeautifulSoup
 if len(sys.argv) != 4:
     print("Usage: python meta_dl.py <hostname> <username> <password>")
-    print("E.g  : python meta_dl.py malware.oregonctf.org gse0 malware")
+    print("E.g  : python meta_dl.py cs201.oregonctf.org demo0 malware")
     exit(0)
 hostname = sys.argv[1]
 username = sys.argv[2]
 passwd = sys.argv[3]
-sets = ['Ch01-2','Ch3.1-3.5','Ch3.6-3.8','Ch3.9-3.11','Ch5-8','Extra-FormatStr']
+sets = ['Ch01-08','Ch11-13','Ch15-16','Ch18-21']
+#sets = ['angr']
 s = requests.Session()
 url='https://'+hostname+'/'
 resp = s.post(url,data={'username':username,'passwd':passwd})
